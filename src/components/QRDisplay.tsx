a33e30a157499d2681277dc1d8d92b18fb7bd712
stoodkev <quentin.correa@gmail.com>
2022-07-12 13:07:57 +0800

    change phrasing

--- src/components/QRDisplay.tsx
+++ src/components/QRDisplay.tsx
@@ -18,9 +18,7 @@ export default ({ op }: Props) => {
         <Card.Subtitle className="mb-2 text-muted">
           Customize your QR Code
         </Card.Subtitle>
-        <Card.Text>
-          Make sure that the QR Code is readable by trying it out on your phone!
-        </Card.Text>
+        <Card.Text>Try scanning the QR Code on your phone!</Card.Text>
         <InputGroup className="mb-3">
           <InputGroup.Text id="basic-addon1">Size</InputGroup.Text>
           <Form.Control
